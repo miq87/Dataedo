ec48006e1f27386bfda6405796069fe5f215cc0f
MIQ87 <miq3l@wp.pl>
2022-04-26 13:58:30 +0200

    interval changed to timer

--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
 import { RandomUserService } from 'src/app/services/random-user.service';
 
 @Component({
   selector: 'app-people',
   templateUrl: './people.component.html'
 })
-export class PeopleComponent implements OnInit {
+export class PeopleComponent implements OnInit, OnDestroy {
 
   person: any
-  interval: any
-  isTimerStarted = false
+  source: any
+  sub: Subscription
+  isTimerPaused = false
 
   constructor(private randomUserService: RandomUserService) { }
 
@@ -17,25 +19,30 @@ export class PeopleComponent implements OnInit {
     this.getRandomUser()
   }
 
-  startTimer(): void {
-    if(this.isTimerStarted) {
-      clearInterval(this.interval)
-      console.log("Timer started")
-      this.interval = setInterval(() => {
-        this.getRandomUser()
-      }, 1500)
-    }
+  startTimer() {
+    this.sub?.unsubscribe()
+    this.source = timer(0, 5000)
+    this.sub = this.source.subscribe({
+      next: (v: any) => {
+        if(!this.isTimerPaused) {
+          this.getRandomUser()
+          console.log(new Date())
+        }
+      },
+      error: () => console.log("Problem with loading peoples")
+    })
   }
 
-  pauseTimer(): void {
-    if(this.isTimerStarted) {
-      clearInterval(this.interval)
-      console.log("Timer paused")
-    }
+  pauseTimer() {
+    this.isTimerPaused = true
   }
 
-  firstStart(): void {
-    this.isTimerStarted = true
+  resumeTimer() {
+    this.isTimerPaused = false
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe()
   }
 
   getRandomUser(): void {
